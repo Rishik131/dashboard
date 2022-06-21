38c9f2725ee2e1d182e5be874b89be79c2082efe
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-22 04:22:16 +0530

    structured

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home/Home";
-import List from "./pages/list/List"
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/Login" element={<Login />} /> */}
-        <Route path="/List" element={<List />} />
-        {/* <Route path="/New" element={<New />} />
-        <Route path="/Single" element={<Single />} /> */}
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="List" element={<List />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="New" element={<New />} />
+          <Route path="Single" element={<Single />} />
+        </Route>
       </Routes>
     </>
   );
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Single.scss"
 
 const Single = () => {
   return (
