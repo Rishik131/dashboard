125f23f76d1f169012f7fbbe25866cca1a0a81b0
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-22 17:22:15 +0530

    widget4

--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import "./widgets.scss"
+import "./widgets.scss";
+import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 
 const Widgets = () => {
   return (
     <div className='widget'>
-        <div className="left">L</div>
-        <div className="right">R</div>
+        <div className="left">
+            <span className="title">USERS</span>
+            <span className="counter">21312</span>
+            <span className="link">see all users</span>
+        </div>
+        <div className="right">
+            <div className="percentage">
+            <ArrowUpwardOutlinedIcon/>
+                20%
+            </div>
+            <PersonOutlinedIcon className="icon"/>
+        </div>
     </div>
   )
 }
