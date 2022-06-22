bb776a2b41265e91eb62b7644274a1890abdf148
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-23 03:04:31 +0530

    widget7

--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -6,17 +6,25 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
 import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
 
-const Widgets = ({type}) => {
-  let data;
-  const amount = 100;
-  const diff = 20;
-    switch(type){
+const Widgets = ({ type }) => {
+    let data;
+    const amount = 100;
+    const diff = 20;
+    switch (type) {
         case "user":
             data = {
                 title: "USERS",
                 isMoney: false,
                 link: "See all users",
-                icon: <PersonOutlinedIcon className="icon"/>
+                icon: (
+                    <PersonOutlinedIcon
+                        className="icon"
+                        style={{
+                            color: "crimson",
+                            backgroundColor: "rgba(255,0,0,.2)",
+                        }}
+                    />
+                )
             };
             break;
         case "order":
@@ -24,7 +32,15 @@ const Widgets = ({type}) => {
                 title: "ORDERS",
                 isMoney: false,
                 link: "view all orders",
-                icon: <ShoppingCartOutlinedIcon className="icon"/>
+                icon: (
+                    <ShoppingCartOutlinedIcon 
+                        className="icon"
+                        style={{
+                            backgroundColor:"rgba(218,165,32,.2)",
+                            color: "goldenrod",
+                        }}
+                    />
+                )
             };
             break;
         case "earning":
@@ -32,7 +48,15 @@ const Widgets = ({type}) => {
                 title: "EARNINGS",
                 isMoney: true,
                 link: "view net earnings",
-                icon: <MonetizationOnOutlinedIcon className="icon"/>
+                icon: (
+                    <MonetizationOnOutlinedIcon 
+                        className="icon" 
+                        style={{
+                            backgroundColor:"rgba(0,128,0,.2)",
+                            color: "green",
+                        }}
+                    />
+                )
             };
             break;
         case "balance":
@@ -40,26 +64,34 @@ const Widgets = ({type}) => {
                 title: "BALANCE",
                 isMoney: true,
                 link: "see details",
-                icon: <AccountBalanceWalletOutlinedIcon className="icon"/>
+                icon: (
+                    <AccountBalanceWalletOutlinedIcon 
+                        className="icon" 
+                        style={{
+                            backgroundColor: "rgba(128,0,128,.2)",
+                            color:"purple",
+                        }}
+                    />
+                )
             };
             break;
     }
     return (
-    <div className='widget'>
-        <div className="left">
-            <span className="title">{data.title}</span>
-            <span className="counter">{data.isMoney && "$"}{amount}</span>
-            <span className="link">{data.link}</span>
-        </div>
-        <div className="right">
-            <div className="percentage positive">
-            <ArrowUpwardOutlinedIcon/>
-                {diff}%
+        <div className='widget'>
+            <div className="left">
+                <span className="title">{data.title}</span>
+                <span className="counter">{data.isMoney && "$"}{amount}</span>
+                <span className="link">{data.link}</span>
             </div>
-            {data.icon}
+            <div className="right">
+                <div className="percentage positive">
+                    <ArrowUpwardOutlinedIcon />
+                    {diff}%
+                </div>
+                {data.icon}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Widgets
