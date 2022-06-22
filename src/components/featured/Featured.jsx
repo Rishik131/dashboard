a6896f0ca9fb343a32d4584a28cd14a318fbc5aa
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-23 03:26:14 +0530

    feature3

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,16 @@
 import "./Featured.scss";
 import React from 'react';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 
 const Featured = () => {
   return (
-    <div className="featured">Featured</div>
+    <div className="featured">
+        <div className="top">
+            <h1 className="title">Total Revenue</h1>
+            <MoreVertOutlinedIcon fontSize="small"/>
+        </div>
+        <div className="bottom"></div>
+    </div>
   )
 }
 
