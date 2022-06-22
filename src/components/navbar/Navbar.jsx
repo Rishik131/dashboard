c0ff4710bc3e8ecc484bcbdbcf482d3b67a3855a
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-22 16:00:24 +0530

    navbar3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./navbar.scss"; 
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleRoundedIcon from '@mui/icons-material/ChatBubbleRounded';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 const Navbar = () => {
   return (
@@ -6,6 +14,28 @@ const Navbar = () => {
         <div className="wrapper">
             <div className="search">
                 <input type="text" placeholder='Search' />
+                <SearchRoundedIcon/>
+            </div>
+            <div className="items">
+                <div className="item">
+                    <LanguageRoundedIcon/>
+                    English
+                </div>
+                <div className="item">
+                    <DarkModeOutlinedIcon/>
+                </div>
+                <div className="item">
+                    <FullscreenExitOutlinedIcon/>
+                </div>
+                <div className="item">
+                    <NotificationsNoneOutlinedIcon/>
+                </div>
+                <div className="item">
+                    <ChatBubbleRoundedIcon/>
+                </div>
+                <div className="item">
+                    <ListOutlinedIcon/>
+                </div>
             </div>
         </div>
     </div>
